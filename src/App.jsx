99ba23fc537f0276bc151b5fed7dfdd204bfca74
Ankit <guptashirley02@gmail.com>
2024-09-05 03:11:54 +0530

    UnderCosntruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import UnderConstruction from "./components/UnderConstruction";
 
 import {
   About,
@@ -17,6 +18,7 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <UnderConstruction />
           <Navbar/>
           <Hero/>
         </div>
---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UnderConstruction = () => {
+  const styles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f0f0f0",
+    fontSize: "5rem",
+    fontWeight: "bold",
+    color: "#ff6347",
+    textAlign: "center",
+    padding: "20px",
+  };
+
+  return (
+    <div style={styles}>
+      Under Construction
+    </div>
+  );
+};
+
+export default UnderConstruction;
