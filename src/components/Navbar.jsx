2e10925be1c568f57b26651d0e9f607936742832
Ankit <guptashirley02@gmail.com>
2024-09-04 22:48:52 +0530

    navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,32 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 
-import { navLinks } from "../constants";
+
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active,setActive] = useState('')
 
   const [toggle,setToggle] = useState(false)
+
+  const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary"
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -29,23 +45,26 @@ const Navbar = () => {
             className="w-9 h-9 objct-contain"
           />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Ankit &nbsp; <span className="sm:block hidden">| Developer</span>{" "}
+            Ankit &nbsp; <span className="sm:block hidden">| Developer</span>
           </p>
         </Link>
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((link) => (
+          {navLinks.map((nav) => (
             <li
-              key={link.id}
+              key={nav.id}
               className={`${
-                active === link.title ? "text-white" : "text-secondary"
+                active === nav.title ? "text-white" : "text-secondary"
               } hover:text-white text-[18px] font-medium cursor-pointer`}
-               onClick={() => setActive(link.title)}
+               onClick={() => setActive(nav.title)}
+// React's state management system recognizes the change in the active state. When the state changes, React triggers a re-render of the component.
+// During this re-render, the className for the list item is recalculated based on the new active state.
             >
-              <a href={`#${link.id}`}>{link.title}</a>
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
+
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img src={toggle ? close : menu}
           alt="menu"
@@ -53,21 +72,21 @@ const Navbar = () => {
           onClick={() => setToggle(!toggle)}
           />
 
-          <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-2-[140px] z-10 rounded-xl`}>
+          <div className={`${!toggle ? 'hidden' : 'flex' } p-6 bg-primary absolute top-20 right-0 mx-4 my-2 min-2-[140px] z-10 rounded-xl`}>
 
           <ul className="list-none flex justify-end items-start flex-col gap-4 ">
-          {navLinks.map((link) => (
+          {navLinks.map((nav) => (
             <li
-              key={link.id}
+              key={nav.id}
               className={`${
-                active === link.title ? "text-white" : "text-secondary"
+                active === nav.title ? "text-white" : "text-secondary"
               } font-poppins font-medium cursor-pointer text-[16px]`}
                onClick={() => {
                 setToggle(!toggle);
-                setActive(link.title)
+                setActive(nav.title)
                }}
             >
-              <a href={`#${link.id}`}>{link.title}</a>
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
