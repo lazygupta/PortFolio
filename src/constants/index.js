4e283b4b25ac523a2bca32a4790bc7238031faf2
Ankit <guptashirley02@gmail.com>
2024-09-06 02:36:35 +0530

    added Experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,6 +17,7 @@ import {
     docker,
     meta,
     starbucks,
+    kites,
     tesla,
     shopify,
     carrent,
@@ -101,9 +102,9 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Software Engineering Intern",
+      company_name: "Kites Foundation Pvt. Ltd.",
+      icon: kites,
       iconBg: "#383E56",
       date: "March 2020 - April 2021",
       points: [
@@ -114,42 +115,15 @@ import {
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
+      title: "Worked on Projects",
+      company_name: "Academics Projects",
       icon: meta,
       iconBg: "#E6DEDD",
       date: "Jan 2023 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Learnt Mysql and learnt to make database and tables.",
+        "I have developed a social media app called TellUs using NextJS and MongoDB as a database.",
+        "Made TimeTable Generator for my college using Android Studio with the Java language"
       ],
     },
   ];
