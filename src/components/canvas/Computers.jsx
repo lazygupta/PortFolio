fb4133061f452b857d3da8c9b6d6ba781de7894e
Ankit <guptashirley02@gmail.com>
2024-09-06 02:47:44 +0530

    shifted pc

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.8, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.6, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
